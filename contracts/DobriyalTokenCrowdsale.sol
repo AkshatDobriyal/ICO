a256289246228708eab30a05966aece9b258e496
AkshatDobriyal <201951016@iiitvadodara.ac.in>
2021-11-13 16:26:07 +0530

    Feat: Added cap to the crowdsale

--- contracts/DobriyalTokenCrowdsale.sol
+++ contracts/DobriyalTokenCrowdsale.sol
@@ -3,16 +3,43 @@ pragma solidity >=0.4.22 <=0.8.10;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract DobriyalTokenCrowdsale is Crowdsale, MintedCrowdsale {
+contract DobriyalTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
+
+    // minimum investor contribution = 0.11 Ether
+    uint256 public investorMinCap = 110000000000000000;
+    mapping(address => uint256) public contributions;
+
     constructor(
         uint256 rate,
         address payable wallet,
-        IERC20 token
+        IERC20 token,
+        uint256 _cap //2712.37 Ether
     )
         Crowdsale(rate, wallet, token)
+        CappedCrowdsale(_cap)
         public
     {
         
+    }
+
+    /**
+    * @dev Extend parent behaviour requiring purchase to respect investor min/max funding cap.
+    * @param _beneficiary Token purchaser
+    * @param _weiAmount Amount of wei contributed
+    */
+
+    function getUserContribution(address _beneficiary) public vie returns (uint256){
+        return contributions[_beneficiary];
+    }
+
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal{
+
+        super._preValidatePurchase(_beneficiary, _weiAmount);
+        uint256 _existingContribution = contributions[_beneficiary];
+        uint256 _newContribution = _existingContribution.add(_weiAmount);
+        require(_newContribution >= investorMinCap);
+        contributions[_beneficiary] = _newContribution;
     }
 }
--- test/DobriyalTokenCrowdsale_test.js
+++ test/DobriyalTokenCrowdsale_test.js
@@ -1,7 +1,9 @@
-import ether from "./helpers/ether";
-
+//const{ ether } = require("./helpers/ether");
+// import EVMRevert from "./helpers/EVMRevert";
+//import "@openzeppelin/contracts/ownership/Ownable.sol";
 const { assert } = require("chai");
 const BigNumber = web3.BigNumber;
+const { Web3 } = require('web3');
 
 require("chai")
     .use(require("chai-bignumber")(BigNumber))
@@ -28,6 +30,8 @@ contract("DobriyalTokenCrowdsale", function([_, wallet, investor1, investor2]) {
         // Crowdsale config
         this.rate = 500;
         this.wallet = wallet;
+        this.cap = ether(100);
+        this.investorMinCap = 110000000000000000;
 
         // deploy crowdsale
         this.crowdsale = await DobriyalTokenCrowdsale.new(
@@ -66,6 +70,13 @@ contract("DobriyalTokenCrowdsale", function([_, wallet, investor1, investor2]) {
         })
     })
 
+    describe("capped crowdsale", function () {
+        it("has the correct hard cap", async function () {
+            const cap = await this.crowdsale.cap();
+            cap.hould.be.bignumber.equal(this.cap);
+        })
+    })
+
     describe("accepting payments", function() {
         it("should accept payments", async function() {
             const value = ether(1);
@@ -74,4 +85,25 @@ contract("DobriyalTokenCrowdsale", function([_, wallet, investor1, investor2]) {
             await this.crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
         })
     })
+
+    describe("buyTokens()", function () {
+        describe("when the contribution is less than the minimum cap", function () {
+            it("rejects the transaction", async function () {
+                const value = this.investorMinCap - 1;
+                await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.rejectedWith(EVMRevert);
+            })
+        })
+    })
+
+    describe("buyTokens()", function () {
+        it("allows the investor to contribute below the minimum cap", async function () {
+            // first contribution is valid
+            const value1 = ether(1);
+            await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+            // second contribution is less than investor cap
+            const value2 = 1; //wei
+            await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 }).should.be .fulfilled;
+        })
+    })
+
 })
