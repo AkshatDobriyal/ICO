c096614c0f1ec524f638370113aaef7e4689661e
AkshatDobriyal <201951016@iiitvadodara.ac.in>
2021-11-13 19:13:10 +0530

    Feat: Added whitelisting for investors

--- contracts/DobriyalTokenCrowdsale.sol
+++ contracts/DobriyalTokenCrowdsale.sol
@@ -4,13 +4,18 @@ pragma solidity >=0.4.22 <=0.8.10;
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/validation/IndividuallyCappedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/validation/WhitelistCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 
-contract DobriyalTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale {
+contract DobriyalTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, IndividuallyCappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale, RefundableCrowdsale {
 
     // minimum investor contribution = 0.11 Ether
     uint256 public investorMinCap = 110000000000000000;
     mapping(address => uint256) public contributions;
+
+    
 
     constructor(
         uint256 rate,
@@ -18,32 +23,27 @@ contract DobriyalTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, 
         IERC20 token,
         uint256 _cap, //2712.37 Ether
         uint256 _openingTime,
-        uint256 _closingTime
+        uint256 _closingTime,
+        uint256 _goal
     )
         Crowdsale(rate, wallet, token)
         CappedCrowdsale(_cap)
         TimedCrowdsale(_openingTime, _closingTime)
         public
     {
-        
+        require(_goal <= _cap);
     }
 
-    /**
-    * @dev Extend parent behaviour requiring purchase to respect investor min/max funding cap.
-    * @param _beneficiary Token purchaser
-    * @param _weiAmount Amount of wei contributed
-    */
-
-    function getUserContribution(address _beneficiary) public view returns (uint256){
+    /*function getUserContribution(address _beneficiary) public view returns (uint256){
         return contributions[_beneficiary];
     }
 
-    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal view {
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
 
         super._preValidatePurchase(_beneficiary, _weiAmount);
         uint256 _existingContribution = contributions[_beneficiary];
         uint256 _newContribution = _existingContribution.add(_weiAmount);
         require(_newContribution >= investorMinCap);
         contributions[_beneficiary] = _newContribution;
-    }
+    }*/
 }
